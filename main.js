ef80aaf32a96662ac034bfcf90fb6c8a539fead2
Clandestine1 <Clandestine.abyss@gmail.com>
2017-05-13 23:38:47 -0400

    Two

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+(function($) {          
+    $(document).ready(function(){                    
+        $(window).scroll(function(){                          
+            if ($(this).scrollTop() > 200) {
+                $('.navbar.navbar-default').fadeIn(500);
+            } else {
+                $('.navbar.navbar-default').fadeOut(500);
+            }
+        });
+    });
+})(jQuery);
+/*http://redvinestudio.com/how-to-make-a-menu-fade-in-on-scroll-using-jquery/*/
 
 
 
