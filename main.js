0d6e02bc5fc98ca1f4127b339220e8edd284e798
Clandestine1 <Clandestine.abyss@gmail.com>
2017-05-14 08:10:51 -0400

    Four

--- main.js
+++ main.js
@@ -11,6 +11,22 @@
 })(jQuery);
 /*http://redvinestudio.com/how-to-make-a-menu-fade-in-on-scroll-using-jquery/*/
 
+function EasyPeasyParallax() {
+	scrollPos = $(this).scrollTop();
+	$('#banner').css({
+		'background-position' : '50% ' + (-scrollPos/4)+"px"
+	});
+	$('#bannertext').css({
+		'margin-top': (scrollPos/4)+"px",
+		'opacity': 1-(scrollPos/250)
+	});
+}
+$(document).ready(function(){
+	$(window).scroll(function() {
+		EasyPeasyParallax();
+	});
+});
+
 
 
 
